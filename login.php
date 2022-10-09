962b3b663e1a737b9c0404faf7313ef083c8fb9f
Yannik Enslin <114471997+yannikonyovel@users.noreply.github.com>
2022-10-09 16:45:52 +0200

    Update login.php

--- login.php
+++ login.php
@@ -1,63 +1,46 @@
-<?php
-session_start();
-require_once "db.php";
-if (isset($_SESSION['user_id']) != "") {
-header("Location: dashboard.php");
-}
-if (isset($_POST['login'])) {
-$email    = mysqli_real_escape_string($conn, $_POST['email']);
-$password = mysqli_real_escape_string($conn, $_POST['password']);
-if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-$email_error = "Please Enter Valid Email ID";
-}
-if (strlen($password) < 6) {
-$password_error = "Password must be minimum of 6 characters";
-}
-$result = mysqli_query($conn, "SELECT * FROM users WHERE email = '" . $email . "' and password = '" . md5($password) . "'");
-if ($row = mysqli_fetch_array($result)) {
-$_SESSION['user_id']     = $row['uid'];
-$_SESSION['user_name']   = $row['name'];
-$_SESSION['user_mobile'] = $row['mobile'];
-$_SESSION['user_email']  = $row['email'];
-header("Location: dashboard.php");
-} else {
-$error_message = "Incorrect Email or Password!!!";
-}
-}
-?>
 <!DOCTYPE html>
-<html lang="de">
+<html>
 <head>
-<meta charset="UTF-8">
-<title>Login to Yovel</title>
-<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+    <meta charset="utf-8"/>
+    <title>Login</title>
+    <link rel="stylesheet" href="style.css"/>
 </head>
 <body>
-<div class="container">
-<div class="row">
-<div class="col-lg-10">
-<div class="page-header">
-<h2>Login</h2>
-</div>
-<p>Please fill all fields in the form</p>
-<span class="text-danger"><?php if (isset($error_message)) echo $error_message; ?></span>
-<form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-<div class="form-group ">
-<label>Email</label>
-<input type="email" name="email" class="form-control" value="" maxlength="30" required="">
-<span class="text-danger"><?php if (isset($email_error)) echo $email_error; ?></span>
-</div>
-<div class="form-group">
-<label>Password</label>
-<input type="password" name="password" class="form-control" value="" maxlength="8" required="">
-<span class="text-danger"><?php if (isset($password_error)) echo $password_error; ?></span>
-</div>
-<input type="submit" class="btn btn-primary" name="login" value="submit">
-<br>
-You don't have account?<a href="registration.php" class="mt-3">Click Here</a>
-</form>
-</div>
-</div>
-</div>
+<?php
+    require('db.php');
+    session_start();
+    // When form submitted, check and create user session.
+    if (isset($_POST['username'])) {
+        $username = stripslashes($_REQUEST['username']);    // removes backslashes
+        $username = mysqli_real_escape_string($con, $username);
+        $password = stripslashes($_REQUEST['password']);
+        $password = mysqli_real_escape_string($con, $password);
+        // Check user is exist in the database
+        $query    = "SELECT * FROM `users` WHERE username='$username'
+                     AND password='" . md5($password) . "'";
+        $result = mysqli_query($con, $query) or die(mysql_error());
+        $rows = mysqli_num_rows($result);
+        if ($rows == 1) {
+            $_SESSION['username'] = $username;
+            // Redirect to user dashboard page
+            header("Location: dashboard.php");
+        } else {
+            echo "<div class='form'>
+                  <h3>Incorrect Username/password.</h3><br/>
+                  <p class='link'>Click here to <a href='login.php'>Login</a> again.</p>
+                  </div>";
+        }
+    } else {
+?>
+    <form class="form" method="post" name="login">
+        <h1 class="login-title">Login</h1>
+        <input type="text" class="login-input" name="username" placeholder="Username" autofocus="true"/>
+        <input type="password" class="login-input" name="password" placeholder="Password"/>
+        <input type="submit" value="Login" name="submit" class="login-button"/>
+        <p class="link"><a href="registration.php">New Registration</a></p>
+  </form>
+<?php
+    }
+?>
 </body>
 </html>
